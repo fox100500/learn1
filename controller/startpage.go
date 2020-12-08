b0e00375e0b0578ee0fa45216b18bcebc958b9fc
fox100500 <spirittime@mail.ru>
2020-12-08 22:45:53 +0300

    dinamic page

--- controller/startpage.go
+++ controller/startpage.go
@@ -1,14 +1,28 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
+	"path/filepath"
+	"text/template"
 
 	"httprouter"
 )
 
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	text := "Приветствую тебя на стартовой странице этого сайта!"
-	//возвращаем простой текст
-	fmt.Fprint(rw, text)
+	//указываем путь к нужному файлу
+	path := filepath.Join("public", "html", "index.html")
+
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	//выводим шаблон клиенту в браузер
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 }
--- controller/users.go
+++ controller/users.go
@@ -1,10 +1,12 @@
 package controller
 
 import (
-	"encoding/json"
-	"../model"
 	"httprouter"
 	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"../model"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -14,8 +16,19 @@ func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-	//возвращаем список клиенту в формате JSON
-	err = json.NewEncoder(rw).Encode(users)
+
+	//указываем путь к файлу с шаблоном
+	main := filepath.Join("public", "html", "usersDynamicPage.html")
+
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(main)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	//исполняем именованный шаблон "users", передавая туда массив со списком пользователей
+	err = tmpl.ExecuteTemplate(rw, "users", users)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
